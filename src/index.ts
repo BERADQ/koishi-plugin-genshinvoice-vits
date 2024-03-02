ee41d1d4719570fb20e75a31e69a037c6e434320
idanran <96647698+idanran@users.noreply.github.com>
2024-03-02 15:13:38 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,28 @@
 import { Context, h, Schema } from "koishi";
 import Vits from "@initencounter/vits";
 import { Language, Voice } from "./list";
-import { IfReplace } from "./IfReplace";
 
 class GenshinVits extends Vits {
-  constructor(ctx: Context, private config: GenshinVits.Config) {
+  static inject = {
+    required: ['http']
+  };
+  constructor(ctx: Context, public config: GenshinVits.Config) {
     super(ctx);
     ctx.command("gsvits <content:text>", "语音生成", { checkArgCount: true })
       .alias("say")
-      .option("speaker", "--spkr [value:string]")
-      .option("language", "--lang [value:string]")
-      .option("length", "--len [value:number]")
-      .option("noise", "--noise [value:number]")
-      .option("noisew", "--noisew [value:number]")
-      .action(async (s, t) => {
-        if (/<.*\/>/gm.test(t)) return "输入的内容不是纯文本。";
-        const { speaker, language, length, noise, noisew } = s.options;
-        const _config = new IfReplace(this.config);
-        _config.ifreplace(
-          "speaker",
-          speaker,
-        )(
-          "language",
-          language,
-        )(
-          "length",
-          length,
-        )(
-          "noise",
-          noise,
-        )(
-          "noisew",
-          noisew,
-        );
-        return await isay(
-          this.ctx,
-          _config.inner,
-          t,
-        );
+      .option("speaker", "--spkr [value:string]", { fallback: config.speaker })
+      .option("language", "--lang [value:string]", { fallback: config.language })
+      .option("length", "--len [value:number]", { fallback: config.length })
+      .option("noise", "[value:number]", { fallback: config.noise })
+      .option("noisew", "[value:number]", { fallback: config.noisew })
+      .option("sdp_ratio", "--sdp [value:number]", { fallback: config.sdp_ratio })
+      .option("text_prompt", "--prompt [value:string]", { fallback: config.text_prompt })
+      .action(async ({ options }, input) => {
+        if (/<.*\/>/gm.test(input)) return "输入的内容不是纯文本。";
+        return await isay(this.ctx, options as Required<typeof options>, input);
       });
   }
   async say(options: Vits.Result): Promise<h> {
-    this.config;
     const speaker = typeof options.speaker_id === "number"
       ? Voice[options.speaker_id]
       : this.config.speaker;
@@ -92,23 +73,23 @@ namespace GenshinVits {
   }
   export const Config: Schema<Config> = Schema.object({
     language: Schema.union(Language).default("ZH").description("默认语言"),
-    speaker: Schema.union(Voice).default("派蒙_ZH").description("默认讲者"),
-    sdp_ratio: Schema.number().min(0).max(1).step(0.1).role("slider").default(
+    speaker: Schema.union(Voice).default("流萤_ZH").description("默认讲者"),
+    sdp_ratio: Schema.number().min(0).max(1).step(0.05).role("slider").default(
       0.5,
     ).description(
       "SDP/DP混合比",
     ),
-    noise: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(0.6)
+    noise: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(0.6)
       .description(
         "感情",
       ),
-    noisew: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(
+    noisew: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(
       0.9,
     ).description(
       "音素长度",
     ),
-    length: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(
-      1.1,
+    length: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(
+      1,
     ).description(
       "语速",
     ),
