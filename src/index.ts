aa2d049b053fe2307ed7f0aabb6c827afe6206ee
idranme <idranme@outlook.com>
2024-07-11 17:14:47 +0800

    1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Context, h, Schema, Quester } from "koishi";
-import Vits from "@initencounter/vits";
-import { Language, Voice } from "./list";
+import { Context, h, Schema, Quester, Service } from "koishi";
+import type Vits from "@initencounter/vits";
+import { Speakers } from "./list";
 import { } from "@koishijs/plugin-help";
 
 interface Predict {
@@ -18,13 +18,13 @@ interface Predict {
   average_duration: number,
 }
 
-class GenshinVits extends Vits {
+class GenshinVits extends Service implements Vits {
   static inject = {
-    required: ['http']
+    required: ["http"]
   };
   constructor(ctx: Context, public config: GenshinVits.Config) {
-    super(ctx);
-    ctx.command("gsvits <content:text>", "语音生成", { checkArgCount: true, checkUnknown: true })
+    super(ctx, "vits", true);
+    ctx.command("gsvits <content:text>", "语音生成", { checkUnknown: true })
       .alias("say")
       .option("speaker", "--spkr [value:string]", { fallback: config.speaker })
       .option("language", "--lang [value:string]", { fallback: config.language, hidden: true })
@@ -34,13 +34,14 @@ class GenshinVits extends Vits {
       .option("sdp_ratio", "--sdp [value:number]", { fallback: config.sdp_ratio })
       .option("text_prompt", "--prompt [value:string]", { fallback: config.text_prompt })
       .action(async ({ options }, input) => {
+        if (!input) return "内容未输入。";
         if (/<.*\/>/gm.test(input)) return "输入的内容不是纯文本。";
         return await isay(this.ctx.http, options as Required<typeof options>, input);
       });
   }
   say(options: Vits.Result): Promise<h> {
     const speaker = typeof options.speaker_id === "number"
-      ? Voice[options.speaker_id]
+      ? Speakers[options.speaker_id]
       : this.config.speaker;
     return isay(this.ctx.http, { ...this.config, speaker }, options.input);
   }
@@ -72,13 +73,12 @@ async function isay(
     ],
     fn_index: 0,
   };
-  // 以前为 v2.genshinvoice.top
   const { data, duration } = await http.post<Predict>(
     "https://bv2.firefly.matce.cn/run/predict",
     payload,
   );
-  const file = data[1].name.replaceAll("\\", "/");
-  return h.audio(`https://bv2.firefly.matce.cn/file=${encodeURI(file)}`, { type: 'voice', duration });
+  const path = data[1].name;
+  return h.audio(`https://bv2.firefly.matce.cn/file=${path}`, { type: "voice", duration });
 }
 
 namespace GenshinVits {
@@ -92,22 +92,22 @@ namespace GenshinVits {
     text_prompt: string,
   }
   export const Config: Schema<Config> = Schema.object({
-    speaker: Schema.union(Voice).default("流萤_ZH").description("发言的角色。"),
-    sdp_ratio: Schema.number().min(0).max(1).step(0.05).role("slider").default(
-      0.5,
+    speaker: Schema.union(Speakers).default("派蒙_ZH").description("发言的角色。"),
+    sdp_ratio: Schema.number().min(0).max(1).step(0.1).role("slider").default(
+      0.2,
     ).description(
       "SDP 在合成时的占比，理论上此比率越高，合成的语音语调方差越大。",
     ),
-    noise: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(0.6)
+    noise: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(0.6)
       .description(
         "样本噪声，控制合成的随机性。",
       ),
-    noisew: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(
-      0.9,
+    noisew: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(
+      0.8,
     ).description(
       "随机时长预测器噪声，控制音素发音长度。",
     ),
-    length: Schema.number().min(0.1).max(2).step(0.05).role("slider").default(
+    length: Schema.number().min(0.1).max(2).step(0.1).role("slider").default(
       1,
     ).description(
       "调节语音长度，相当于调节语速，该数值越大语速越慢。",
@@ -115,7 +115,7 @@ namespace GenshinVits {
     text_prompt: Schema.string().default("Happy").description(
       "用文字描述生成风格，注意只能使用英文且首字母大写单词。",
     ),
-    language: Schema.union(Language).default("ZH").description("语言。").hidden(),
+    language: Schema.union(["ZH"] as string[]).default("ZH").description("语言。").hidden(),
   });
 }
 
